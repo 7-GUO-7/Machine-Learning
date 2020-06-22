bcf13e19a4dc57de4697d0335cce38b7a7b7e5ef
7-GUO-7 <7_GUO_7@sjtu.edu.cn>
2020-06-22 13:17:29 +0800

    update resnet training

--- dataloader.py
+++ dataloader.py
@@ -1,12 +1,21 @@
+import os
+import shutil
+
+import numpy as np
+from PIL import Image
+
 import torch
 from torch.autograd import Variable
-from torchvision import transforms
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
-import numpy as np
-import os
 
+# from torchvision import models
+from torchvision import transforms
 class TorchDataset():
-    def __init__(self, filename="train_labels.txt", data_dir="train", resize_height=None, resize_width=None, repeat=1):
+    def __init__(self, filename1="train_labels.txt", filename2 = "train_labels.txt",data_dir="dataset/train/", resize_height=None, resize_width=None, repeat=1):
+
         '''
         :param filename: 数据文件TXT：格式：imge_name.jpg label1_id labe2_id
         :param image_dir: 图片路径：image_dir+imge_name.jpg构成图片的完整路径
@@ -15,18 +24,24 @@ class TorchDataset():
                               PS：当参数resize_height或resize_width其中一个为None时，可实现等比例缩放
         :param repeat: 所有样本数据重复次数，默认循环一次，当repeat为None时，表示无限循环<sys.maxsize
         '''
-        self.image_label_list = self.read_file(filename)
         self.image_dir = data_dir
+        self.image_label_list, self.image_path_list = self.read_file(filename1,filename2)
+
         self.len = len(self.image_label_list)
         self.repeat = repeat
         self.resize_height = resize_height
         self.resize_width = resize_width
+        self.transform = transforms.Compose([
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ])
 
         # 相关预处理的初始化
         '''class torchvision.transforms.ToTensor'''
         # 把shape=(H,W,C)的像素值范围为[0, 255]的PIL.Image或者numpy.ndarray数据
         # 转换成shape=(C,H,W)的像素数据，并且被归一化到[0.0, 1.0]的torch.FloatTensor类型。
-        self.toTensor = transforms.ToTensor()
+        # self.toTensor = transforms.ToTensor()
 
         '''class torchvision.transforms.Normalize(mean, std)
         此转换类作用于torch. * Tensor,给定均值(R, G, B) 和标准差(R, G, B)，
@@ -39,32 +54,71 @@ class TorchDataset():
         # print("i={},index={}".format(i, index))
         data_name, label = self.image_label_list[index]
         # print(data_name, label)
-        data_path = os.path.join(self.image_dir, data_name)
-        data = self.load_data(data_path, self.resize_height, self.resize_width, normalization=False)
-        data = self.data_preproccess(data)
-        label = np.array(label)
-        return data, label
+        img_path = self.image_path_list[index]
+        img = Image.open(img_path)
+        img = self.transform(img)
+        # print(img)
+        # data_path = os.path.join(self.image_dir, data_name)
+        # data = self.load_data(data_path, self.resize_height, self.resize_width, normalization=False)
+        # data = self.data_preproccess(img)
+        # label = np.array(label)
+        return img, label
 
     def __len__(self):
         if self.repeat == None:
-            data_len = 1000
+            data_len = 10000000
         else:
             data_len = len(self.image_label_list) * self.repeat
         return data_len
 
-    def read_file(self, filename):
+    def read_file(self, filename,filename2):
         image_label_list = []
+        image_path_list = []
         with open(filename, 'r') as f:
             lines = f.readlines()
             for line in lines:
                 # rstrip：用来去除结尾字符、空白符(包括\n、\r、\t、' '，即：换行、回车、制表符、空格)
                 content = line.rstrip().split(' ')
                 name = content[0]
+
+                pic_path = self.image_dir + name # name is pic folder
+                pic_list = [x for x in os.listdir(pic_path)]
+                for it in pic_list:
+                    image_path_list.append(pic_path+'/'+it)
+                number = len(pic_list)
+
                 labels = []
                 for value in content[1:]:
                     labels.append(int(value))
-                image_label_list.append((name, labels))
-        return image_label_list
+                label = [0. for i in range(10)]
+                for i in labels:
+                    label[i] = 1.
+                for i1 in range(number):
+                    image_label_list.append((name, torch.FloatTensor(label)))
+        with open(filename2, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                 # rstrip：用来去除结尾字符、空白符(包括\n、\r、\t、' '，即：换行、回车、制表符、空格)
+                content = line.rstrip().split(' ')
+                name = content[0]
+
+                pic_path = self.image_dir + name # name is pic folder
+                pic_list = [x for x in os.listdir(pic_path)]
+                for it in pic_list:
+                    image_path_list.append(pic_path+'/'+it)
+                number = len(pic_list)
+
+                labels = []
+                for value in content[1:]:
+                    labels.append(int(value))
+                label = [0. for i in range(10)]
+                for i in labels:
+                    label[i] = 1.
+                for i1 in range(number):
+                    image_label_list.append((name, torch.FloatTensor(label)))
+        # print(image_label_list)
+        # print(image_path_list)
+        return image_label_list, image_path_list
 
     def load_data(self, path, resize_height, resize_width, normalization):
         '''
@@ -78,9 +132,9 @@ class TorchDataset():
         file_list = [name for name in os.listdir(path)]
         l = []
         for it in file_list:
-            y = np.loadtxt(path + '/' + str(it), delimiter=',').reshape(1, 2048)
+            y = np.loadtxt(path + str(it), delimiter=',').reshape(1, 2048)
             l.append(y)
-        return np.array(l)
+        return np.array(l, dtype='float32')
 
     def data_preproccess(self, data):
         '''
@@ -88,16 +142,14 @@ class TorchDataset():
         :param data:
         :return:
         '''
-        data = self.toTensor(data)
+        # data = self.toTensor(data)
         return data
 
-batch_size = 1
-train_loader = TorchDataset(repeat=1)
+# train_loader = TorchDataset(repeat=1)
 # test_loader = DataLoader(dataset=test_data, batch_size=batch_size,shuffle=False)
 
-epoch_num = 2
-for epoch in range(epoch_num):
-    for batch_data, batch_label in train_loader:
-        data = batch_data[0,:]
-        print(data.shape, batch_label)
-
+# epoch_num = 2
+# for epoch in range(epoch_num):
+#     for batch_data, batch_label in train_loader:
+#         # data = batch_data[0,:]
+#         print(batch_data.shape, batch_label)
---
+++ main.py
@@ -0,0 +1,204 @@
+
+import numpy as np
+
+import argparse
+import torch
+
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+from torch.utils.data import Dataset
+import torch.utils.data as data_utils
+from torch.autograd import Variable
+from tqdm import tqdm
+
+from dataloader import TorchDataset
+from model import Attention, GatedAttention
+from torchvision import models
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST bags Example')
+parser.add_argument('--epochs', type=int, default=20, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--reg', type=float, default=10e-5, metavar='R',
+                    help='weight decay')
+parser.add_argument('--target_number', type=int, default=9, metavar='T',
+                    help='bags have a positive labels if they contain at least one 9')
+parser.add_argument('--mean_bag_length', type=int, default=10, metavar='ML',
+                    help='average bag length')
+parser.add_argument('--var_bag_length', type=int, default=2, metavar='VL',
+                    help='variance of bag length')
+parser.add_argument('--num_bags_train', type=int, default=200, metavar='NTrain',
+                    help='number of bags in training set')
+parser.add_argument('--num_bags_test', type=int, default=50, metavar='NTest',
+                    help='number of bags in test set')
+parser.add_argument('--seed', type=int, default=2, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--model', type=str, default='attention', help='Choose b/w attention and gated_attention')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+# args.cuda = True
+
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+    print('\nGPU is ON!')
+
+print('Load Train and Test Set')
+loader_kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+
+train_loader = data_utils.DataLoader(TorchDataset(),
+    # target_number=args.target_number,
+    #                                            mean_bag_length=args.mean_bag_length,
+    #                                            var_bag_length=args.var_bag_length,
+    #                                            num_bag=args.num_bags_train,
+    #                                            seed=args.seed,
+    #                                            train=True),
+                                     batch_size=1,
+                                     shuffle=True,
+                                     **loader_kwargs)
+
+# test_loader = data_utils.DataLoader(MnistBags(target_number=args.target_number,
+#                                               mean_bag_length=args.mean_bag_length,
+#                                               var_bag_length=args.var_bag_length,
+#                                               num_bag=args.num_bags_test,
+#                                               seed=args.seed,
+#                                               train=False),
+#                                     batch_size=1,
+#                                     shuffle=False,
+#                                     **loader_kwargs)
+
+print('Init Model')
+if args.model=='attention':
+    model = models.resnet50(pretrained=True)
+    # for parma in model.parameters():
+    #     parma.requires_grad = False
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, 10)
+
+
+    # model = Attention()
+    # model = torch.load('model40.pth')
+elif args.model=='gated_attention':
+    model = GatedAttention()
+if args.cuda:
+    model.cuda()
+
+optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.999), weight_decay=args.reg)
+
+def calculate_objective(model, X, Y):
+    # Y = Y.float()
+    Y = Y.float()
+    loss = nn.BCELoss(reduction ='sum')
+    Y_prob = model(X)
+
+    ###softmax
+
+    Y_prob = torch.sigmoid(Y_prob)
+    Y_prob = torch.clamp(Y_prob, min=1e-6, max=1. - 1e-6)
+    # Y_prob = torch.clamp(Y_prob, min=1e-5, max=1. - 1e-5)
+    print(Y_prob)
+    # print(Y)
+    output = loss(Y_prob, Y)
+    # print(output.item())
+    return output
+
+# AUXILIARY METHODS
+def calculate_classification_error(model, X, Y):
+    # Y = Y.float()
+    Y_prob = model(X)
+    Y_prob = torch.sigmoid(Y_prob)[0]
+    Y = Y[0]
+    Y_hat = []
+    # p = np.sum(np.array(Y))
+    # print(p)
+    for i in range(len(Y_prob)):
+        Y_hat.append(torch.ge(Y_prob[i], 0.5).float())
+    error = 0
+    for i in range(len(Y_hat)):
+        if int(Y_hat[i]) != int(Y[i]):
+            error += 1
+            break
+    #     error += 1. - Y_hat[i].eq(Y[i].float).cpu().float().mean().item()
+
+    return error,Y_prob,Y_hat
+
+def train(epoch):
+    model.train()
+    train_loss = 0.
+    loss = torch.nn.BCELoss()
+    train_error = 0.
+    for batch_idx, (data, label) in enumerate(tqdm(train_loader)):
+        # print(data)
+        # print(label)
+        bag_label = label
+        if args.cuda:
+            data, bag_label = data.cuda(), bag_label.cuda()
+        data, bag_label = Variable(data), Variable(bag_label)
+
+        # reset gradients
+        optimizer.zero_grad()
+        loss = calculate_objective(model, data, bag_label)
+        print(loss)
+        # print(data)
+        # print()
+        # print(bag_label)
+        train_loss += loss.data.item()
+        error, _, _ = calculate_classification_error(model, data, bag_label)
+        train_error += error
+        # backward pass
+        loss.backward()
+        # step
+        optimizer.step()
+
+
+        # calculate loss and metrics
+
+
+    # calculate loss and error for epoch
+    train_loss /= len(train_loader)
+    train_error /= len(train_loader)
+    torch.save(model, 'models/model%i.pth'%(epoch))
+    print('Epoch: {}, Loss: {:.4f}, Train error: {:.4f}'.format(epoch, train_loss, train_error))
+
+
+# def test():
+#     model.eval()
+#     test_loss = 0.
+#     test_error = 0.
+#     for batch_idx, (data, label) in enumerate(test_loader):
+#         bag_label = label[0]
+#         instance_labels = label[1]
+#         if args.cuda:
+#             data, bag_label = data.cuda(), bag_label.cuda()
+#         data, bag_label = Variable(data), Variable(bag_label)
+#         loss, attention_weights = model.calculate_objective(data, bag_label)
+#         test_loss += loss.data[0]
+#         error, predicted_label = model.calculate_classification_error(data, bag_label)
+#         test_error += error
+
+#         if batch_idx < 5:  # plot bag labels and instance labels for first 5 bags
+#             bag_level = (bag_label.cpu().data.numpy()[0], int(predicted_label.cpu().data.numpy()[0][0]))
+#             instance_level = list(zip(instance_labels.numpy()[0].tolist(),
+#                                  np.round(attention_weights.cpu().data.numpy()[0], decimals=3).tolist()))
+
+#             print('\nTrue Bag Label, Predicted Bag Label: {}\n'
+#                   'True Instance Labels, Attention Weights: {}'.format(bag_level, instance_level))
+
+#     test_error /= len(test_loader)
+#     test_loss /= len(test_loader)
+
+#     print('\nTest Set, Loss: {:.4f}, Test error: {:.4f}'.format(test_loss.cpu().numpy()[0], test_error))
+
+
+if __name__ == "__main__":
+    print('Start Training')
+    for epoch in range(1, args.epochs + 1):
+        train(epoch)
+    print('Start Testing')
+    # test()
